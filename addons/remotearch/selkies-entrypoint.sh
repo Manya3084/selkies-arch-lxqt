d2143cc761599de941f2d0392ef844b28b7d2edf
Manya3084 <104078862+Manya3084@users.noreply.github.com>
2026-08-19 22:47:46 +1000

    Add Arch LXQt Dockerfile and entrypoints

---
+++ addons/remotearch/selkies-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+set -e
+
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/tmp/runtime-ubuntu}"
+
+# Load the shared session environment computed by container-entrypoint.sh
+[ -f "${XDG_RUNTIME_DIR}/container-env" ] && . "${XDG_RUNTIME_DIR}/container-env"
+
+if [ "${SELKIES_WAYLAND:-false}" != "true" ]; then
+  export DISPLAY="${DISPLAY:-:20}"
+  echo 'Waiting for X Socket'
+  until [ -S "/tmp/.X11-unix/X${DISPLAY#*:}" ]; do sleep 0.5; done
+  echo 'X Server is ready'
+  selkies-resize 1920x1080
+fi
+
+exec selkies --addr=0.0.0.0 --port="${SELKIES_PORT:-8080}" "$@"
